f156e3243ad55ecec4c20ecb7884af611644e84b
byulsdeep <109781929+byulsdeep@users.noreply.github.com>
2023-03-23 02:20:22 +0900

    Update SquirrelBook.js

--- SquirrelBook.js
+++ SquirrelBook.js
@@ -7,25 +7,55 @@ $(() => {
       this.data = data
       init()
     },
+    error: e => $('section').append(e),
   })
 })
 function init() {
-  $('section').append(makeContact())
-  $('#friends').on('click', moveFriendlist())
+  // $('section').append(makeContact())
+  $('#home').on('click', moveHome)
+  $('#contacts').on('click', moveContacts)
+  $('#friends').on('click', moveFriendlist)
+  $('#lottery').on('click', moveLottery)
+  $('#rps').on('click', moveRps)
+  $('#searchButton').on('click', moveSearch)
+  $('.track').on('click', moveSong).css('cursor', 'pointer')
+}
+function moveSong() {
+  $('section').html($($(this).children()[0]).clone().css('display', 'initial'))
+}
+function moveSearch() {
+  $('section').html(makeSearchResult())
+}
+function moveContacts() {
+  $('section').html(makeContact())
+}
+function moveLottery() {
+  $('section').html(makeLottery())
+}
+function moveRps() {
+  $('section').html(makeRps())
+}
+function moveHome() {
+  $('section').html(makeCarousel())
 }
 function moveFriendlist() {
-    $('section').html(makeFriendlist())
+  $('section').html(makeFriendlist())
 }
-function makeFriendlist() {
+function makeSearchResult() {
   const table = $('<table>').addClass('table')
   const thead = $('<tr>')
   Object.keys(this.data.contacts[0]).forEach(key =>
     thead.append(key === 'no' ? null : $('<th>').text(key))
   )
-  thead.append($('<th>').text('❤'))
+  thead.append($('<th>').text('🤍'))
   table.append(thead)
   $(this.data.contacts).each((i, contact) => {
-    if (this.friendlist.includes(contact.no)) {
+    // console.log(contact.name)
+    if (
+      contact.name.includes($('#searchBox').val()) ||
+      contact.tel.includes($('#searchBox').val()) ||
+      contact.address.includes($('#searchBox').val())
+    ) {
       const tr = $('<tr>')
       Object.keys(contact).forEach(key => {
         tr.append(
@@ -46,14 +76,14 @@ function makeFriendlist() {
         $('<td>')
           .append(
             button
-              .text(friendlist.includes(contact.no) ? '🤍' : '❤')
+              .text(friendlist.includes(contact.no) ? '❤' : '🤍')
               .on('click', () => {
                 if (friendlist.includes(contact.no)) {
                   friendlist.splice(friendlist.indexOf(contact.no), 1)
                 } else {
                   friendlist.push(contact.no)
                 }
-                button.text(button.text() === '🤍' ? '❤' : '🤍')
+                button.text(button.text() === '❤' ? '🤍' : '❤')
               })
           )
           .append(
@@ -71,7 +101,67 @@ function makeFriendlist() {
               })
           )
       )
-      if (contact.name === 'Aki Cruz') tr.addClass('bg-danger')
+      table.append(tr)
+    }
+  })
+  return table
+}
+function makeLottery() {
+  return null
+}
+function makeRps() {
+  return null
+}
+function makeCarousel() {
+  return null
+}
+function makeFriendlist() {
+  const table = $('<table>').addClass('table')
+  const thead = $('<tr>')
+  Object.keys(this.data.contacts[0]).forEach(key =>
+    thead.append(key === 'no' ? null : $('<th>').text(key))
+  )
+  thead.append($('<th>').text('❤'))
+  table.append(thead)
+  $(this.data.contacts).each((i, contact) => {
+    if (friendlist.includes(contact.no)) {
+      const tr = $('<tr>')
+      Object.keys(contact).forEach(key => {
+        tr.append(
+          key === 'no'
+            ? null
+            : $('<td>').append(
+                key === 'photo'
+                  ? $('<img>', {
+                      src: contact[key],
+                      width: '128px',
+                    }).addClass('img-fluid img-thumbnail')
+                  : contact[key]
+              )
+        )
+      })
+      const button = $('<button>').addClass('btn btn-light')
+      tr.append(
+        $('<td>')
+          .append(
+            button.text('❤').on('click', () => {
+              friendlist.splice(friendlist.indexOf(contact.no), 1)
+              tr.remove()
+            })
+          )
+          .append(
+            $('<button>')
+              .addClass('btn btn-light')
+              .text('❌')
+              .on('click', () => {
+                friendlist.splice(friendlist.indexOf(contact.no), 1)
+                this.data.contacts = this.data.contacts.filter(
+                  c => contact.no !== c.no
+                )
+                tr.remove()
+              })
+          )
+      )
       table.append(tr)
     }
   })
@@ -83,7 +173,7 @@ function makeContact() {
   Object.keys(this.data.contacts[0]).forEach(key =>
     thead.append(key === 'no' ? null : $('<th>').text(key))
   )
-  thead.append($('<th>').text('❤'))
+  thead.append($('<th>').text('🤍'))
   table.append(thead)
   $(this.data.contacts).each((i, contact) => {
     const tr = $('<tr>')
@@ -106,14 +196,14 @@ function makeContact() {
       $('<td>')
         .append(
           button
-            .text(friendlist.includes(contact.no) ? '🤍' : '❤')
+            .text(friendlist.includes(contact.no) ? '❤' : '🤍')
             .on('click', () => {
               if (friendlist.includes(contact.no)) {
                 friendlist.splice(friendlist.indexOf(contact.no), 1)
               } else {
                 friendlist.push(contact.no)
               }
-              button.text(button.text() === '🤍' ? '❤' : '🤍')
+              button.text(button.text() === '❤' ? '🤍' : '❤')
             })
         )
         .append(
@@ -131,7 +221,6 @@ function makeContact() {
             })
         )
     )
-    if (contact.name === 'Aki Cruz') tr.addClass('bg-danger')
     table.append(tr)
   })
   return table
